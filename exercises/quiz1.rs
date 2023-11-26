94392ab75a92bfceb3e78aa89a1600823e0a5023
Marsevil <marsevil@hotmail.fr>
2023-11-26 20:51:26 +0100

    quiz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(nb_apples: u32) -> u32 {
+    const PRICE_BREAK: u32 = 40;
+    let apple_price = if nb_apples > PRICE_BREAK {
+        1
+    } else {
+        2
+    };
+    
+    let price = nb_apples * apple_price;
+    price
+}
 
 // Don't modify this function!
 #[test]
